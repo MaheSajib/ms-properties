b5b480f8c96fb4fc5076030109b04a86499b1357
MaheSajib <mahe.sajib@gmail.com>
2022-02-09 14:06:32 +0600

    design perpous react project

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/img-5.jpg';
+
+export const InfoData = {
+    heading: 'Explore our beautiful home',
+    paragraphOne: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium debitis ad architecto corporis aperiam vel accusantium excepturi dolores saepe inventore!',
+    paragraphTwo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem velit accusamus veritatis quis.',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium debitis',
+    paragraphTwo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.veritatis quis.',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,66 @@
+import ImageOne from '../images/img-1.jpg';
+import ImageTwo from '../images/img-2.jpg';
+import ImageThree from '../images/img-3.jpg';
+import ImageFour from '../images/img-4.jpg';
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia',
+        price: '$4,280,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House',
+    },
+    {
+        title: 'House in Calabasas, California',
+        price: '$6,980,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House',
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,580,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House',
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$7,543,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House',
+    },
+];
+
+// export const SliderDataTwo = [
+//     {
+//         title: 'House in Las Vegas, Nevada',
+//         price: '$8,980,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageFive,
+//         alt: 'House',
+//     },
+//     {
+//         title: 'House in Houston, Texas',
+//         price: '$3,460,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageSix,
+//         alt: 'House',
+//     },
+//     {
+//         title: 'House in Alajuela, Costa Rica',
+//         price: '$5,420,000',
+//         path: '/home',
+//         label: 'View Home',
+//         image: ImageSeven,
+//         alt: 'House',
+//     }
+// ];
